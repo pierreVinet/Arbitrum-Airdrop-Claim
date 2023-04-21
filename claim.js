ee81733b17a10ab42cc51d3a91e891e908e3962d
Pierre <pierre.vinet17@gmail.com>
2023-04-22 00:55:59 +0200

    up

--- claim.js
+++ claim.js
@@ -1,7 +1,6 @@
 // Setup: npm install @alch/alchemy-sdk
 const { Network, Alchemy } = require("alchemy-sdk");
 
-// Optional Config object, but defaults to demo api-key and eth-mainnet.
 const settings = {
   apiKey: process.env.ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
   network: Network.ARB_MAINNET, // Replace with your network.
@@ -9,9 +8,9 @@ const settings = {
 
 const alchemy = new Alchemy(settings);
 
-async function main() {
+const claim = async () => {
   const latestBlock = await alchemy.core.getBlockNumber();
   console.log("The latest block number is", latestBlock);
-}
+};
 
-main();
+claim();
